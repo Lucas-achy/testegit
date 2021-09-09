54671c861115058dd9c744796ef3475f2b88411c
Lucas-achy <89885355+Lucas-achy@users.noreply.github.com>
2021-09-09 14:05:32 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', (req, res )=>{
     res.status(200).send({message: 'Ola mundo estou funcionando!'});
 })
 
+app.get('/Olá', (req, res )=>{
+    res.status(200).send({message: 'Oi eu sou a rota Olá'});
+})
+
 app.listen(3001, ()=>{
     console.log('Api rodando na porta 3001')
 })
